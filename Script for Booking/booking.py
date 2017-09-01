cfbd63f2227e65277a7445798b19564e1fd361bf
ysalfafara <ysalfafara@gmail.com>
2017-09-01 19:49:23 +0800

    Script for Booking

---
+++ Script for Booking/booking.py
@@ -0,0 +1,95 @@
+# tripadvisor Scrapper - use this one to scrape hotels
+
+# importing libraries
+from bs4 import BeautifulSoup
+import urllib
+import os
+import re
+from urllib.request import urlopen
+
+# creating CSV file to be used
+
+file = open(os.path.expanduser(r"~/Desktop/Booking Reviews.csv"), "wb")
+file.write(
+    b"Organization,Reviewer,Address,Review Title,Review,Rating Date,Rating" + b"\n")
+
+# List the first page of the reviews (ends with "#tab-reviews") - separate the websites with ,
+WebSites = [
+    "https://www.booking.com/hotel/ph/taal-vista.en-gb.html#tab-reviews"]
+Checker = "REVIEWS"
+
+# looping through each site until it hits a break
+num = 0
+for theurl in WebSites:
+    thepage = urlopen(theurl)
+    soup = BeautifulSoup(thepage, "html.parser")
+    while True:
+        # extract the help count, restaurant review count, attraction review count and hotel review count
+        a = b = 0
+        helpcountarray = hotelarray = ""
+        WebSites1 = ""
+
+        for profile in soup.findAll(attrs={"class": "review_item_review"}):
+            image = profile.text.replace("\n", "|||||").strip()
+            if image.find("helpful vote") > 0:
+                counter = image.split("helpful vote", 1)[0].split("|", 1)[1][-4:].replace("|", "").strip()
+                if len(helpcountarray) == 0:
+                    helpcountarray = [counter]
+                else:
+                    helpcountarray.append(counter)
+            elif image.find("helpful vote") < 0:
+                if len(helpcountarray) == 0:
+                    helpcountarray = ["0"]
+                else:
+                    helpcountarray.append("0")
+
+        Organization = "Taal Vista Hotel"
+
+                # Loop through each review on the page
+        for x in range(0, len(helpcountarray)):
+            try:
+                User = soup.findAll(attrs={"class": "reviewer_country"})[x].text
+                Reviewer = User[0]
+            except:
+                Reviewer = "N/A"
+                continue
+
+            Reviewer = Reviewer.replace(',', ' ').replace('"', '').replace('"', '').replace('"', '').strip()
+            Address = soup.findAll(attrs={"class": "reviewer_country"})[x].text.replace(',', ' ').replace('\n', ' ').strip()
+            ReviewTitle = soup.findAll(attrs={"class": "review_item_header_content"})[x].text.replace(',', ' ').replace('"', '').replace('"','').replace('"', '').replace('e', 'e').strip()
+            RatingDate = soup.findAll(attrs={"class": "review_item_date"})[x].text.replace('Reviewed', ' ').replace('NEW',' ').replace(',', ' ').strip()
+            Rating = soup.findAll(attrs={"class": "review-score-badge"})[x].text.replace(',', ' ').replace('\n', ' ').strip()
+            Review = soup.findAll(attrs={"class": "review_neg"})[x].text.replace(',', ' ').replace('\n', ' ').strip()
+
+            Record = Organization + "," + Reviewer + "," + Address +  "," + ReviewTitle + "," + Review + "," + RatingDate + "," + Rating
+            if Checker == "REVIEWS":
+                file.write(bytes(Record, encoding="ascii", errors='ignore')  + b"\n")
+
+            Review = soup.findAll(attrs={"class": "review_pos"})[x].text.replace(',', ' ').replace('\n', ' ').strip()
+
+            Record = Organization + "," + Reviewer + "," + Address +  "," + ReviewTitle + "," + Review + "," + RatingDate + "," + Rating
+            if Checker == "REVIEWS":
+                file.write(bytes(Record, encoding="ascii", errors='ignore')  + b"\n")
+
+        link = soup.find_all('a', attrs={"id":"review_next_page_link"})
+        print(Organization)
+        print(link)
+        if link == []:
+            num = num + 10
+            if num%2 == 0:
+                Website1 = "https://www.booking.com" + "/reviewlist.en-gb.html?aid=304142;label=gen173nr-1FCAEoggJCAlhYSDNiBW5vcmVmaLQBiAEBmAEuwgEKd2luZG93cyAxMMgBDNgBAegBAfgBC5ICAXmoAgM;sid=57e16a7fc2b9e71d3324af8b746657c1;cc1=ph;dist=1;pagename=taal-vista;r_lang=en;type=total&;offset=" + str(num) + ";rows=10"
+                page = urlopen(WebSites1)
+                soup = BeautifulSoup(page, "html.parser")
+                print(WebSites1)
+                Checker = WebSites1[-7:]
+            else:
+                WebSites1 = "https://www.booking.com" + "/reviewlist.en-gb.html?aid=304142;label=gen173nr-1FCAEoggJCAlhYSDNiBW5vcmVmaLQBiAEBmAEuwgEKd2luZG93cyAxMMgBDNgBAegBAfgBC5ICAXmoAgM;sid=57e16a7fc2b9e71d3324af8b746657c1;cc1=ph;dist=1;pagename=taal-vista;r_lang=en;type=total;upsort_photo=0&;offset=" + str(num) + ";rows=10"
+                page = urlopen(WebSites1)
+                soup = BeautifulSoup(page, "html.parser")
+                print(WebSites1)
+                Checker = WebSites1[-7:]
+        else:
+            break
+
+
+file.close()
