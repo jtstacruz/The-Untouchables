f3c390bd2d4c315a7fb3575235dbb4ff34994345
ysalfafara <ysalfafara@gmail.com>
2017-09-01 19:55:53 +0800

    Updated script for agoda
    Extract > Insert to Databse > Preprocess > Insert to Databse > Count
    Occurrence

--- Script for Agoda/agoda.py
+++ Script for Agoda/agoda.py
@@ -6,7 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import pymysql
 import os
-
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
+import re
 #connect to mysql
 db = pymysql.connect("localhost", "root", "", "reviewer")
 cursor = db.cursor()
@@ -16,7 +19,7 @@ file = open(os.path.expanduser(r"~/Desktop/Agoda Reviews.csv"), "wb")
 file.write(
     b"Review,Rating Date,Rating  " + b"\n")
 
-#extract booking and insert to database
+#extract agoda and insert to database
 def agoda():
     browser = webdriver.Chrome()
     browser.get('https://www.agoda.com/taal-vista-hotel/hotel/tagaytay-ph.html')
@@ -30,6 +33,9 @@ def agoda():
     count = 0
     agoda_id = 100
     while True:
+        #######################################
+        ####### EXTRACT DATA FROM AGODA #######
+        #######################################
         Review_element = browser.find_elements_by_xpath("//*[@data-selenium='reviews-comments']")
         Rating_date_element = browser.find_elements_by_xpath("//*[@data-selenium='review-date']")
         Rating_element = browser.find_elements_by_xpath("//*[@data-selenium='individual-review-rate']")
@@ -38,7 +44,9 @@ def agoda():
         Rating_date = []
         Rating = []
 
-        for x in range(10):
+        for x in range(5):
+            if count == 5:
+                browser.quit()
 
             Review.append(Review_element[x].text.replace(',', ' ').replace('"', '').replace('"', '').replace('"', '').replace('\n', ' ').strip())
             Rating.append(Rating_element[x].text.replace('.','').strip())
@@ -47,18 +55,81 @@ def agoda():
             #print at cmd
             print(Review[count] + Rating[count] + Rating_date[count])
 
-            #insert to database (negative reviews)
+            ################################
+            ###### INSERT TO DATABASE ######
+            ################################
             cursor.execute("INSERT INTO CUSTOMER  (REVIEWSITES_ID, CSTMR_REVIEW, CSTMR_RATINGDATE, CSTMR_RATING) values (%s,%s,%s,%s)",
             (agoda_id, str(Review[count]),str(Rating_date[count]),str(Rating[count])))
 
             db.commit()
-            count = count + 1
 
+            ###########################
+            ###### PREPROCESSING ######
+            ###########################
+            emoji_pattern = re.compile("["
+                u"\U0001F600-\U0001F64F"         # emoticons
+                u"\U0001F300-\U0001F5FF"         # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"         # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"         # flags (iOS)
+                           "]+", flags=re.UNICODE)
+            rx = re.compile('\W+')
+            stop_words = set(stopwords.words("english"))
+
+
+            first = emoji_pattern.sub(r'', Review[count])   #replace all emoji to blank space
+            second = re.sub(r"http\S+", "", first)          #replace URL into blank space
+            third = rx.sub(' ', second).strip()             #replace all non-alphanumerics to spaces
+            words = word_tokenize(third.lower())            #tokenize and lower case
+            filtered_sentence = []
+
+            for w in words:
+              if w not in stop_words:
+                  filtered_sentence.append(w)
+            print ("*************************************")
+            print ("**********  CLEANED DATA  ***********")
+            print ("*************************************")
+            print ("*************************************")
+            print ("".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in filtered_sentence]).strip())
+
+
+
+            #############################################
+            ###### INSERT TO DATABASE PREPROCESSED ######
+            #############################################
+            cursor.execute("INSERT INTO  PREPROCESSED(PREPROCESSED_REVIEW) values (%s)", (str(Review[count])))
+            #commit insert
             db.commit()
-            count = count + 1
 
-            if count == 5:
-                break
+            ###############################
+            ###### COUNT OCCURENECE  ######
+            ###############################
+            words_pos = ["clean", "rooms", "amazing", "worth", "great", "luxury", "kind", "yummy", "better", "comfortable"]
+            words_neg = ["old", "No", "horrible", "smelly", "failed", "disgusting", "poor", "not", "stains", "outdated", "noisy", "although" ]
+            pos = 0
+            neg = 0
+
+            for word in Review[count]:
+            	words.append(word)
+
+            for item in words:
+            	if item in words_pos:
+            		pos += 1
+
+            for item in words:
+            	if item in words_neg:
+            		neg += 1
+
+
+            if pos > neg:
+            	print("POSITIVE!" + " : " + str(pos))
+            elif neg > pos:
+            	print("NEGATIVE" + " : " + str(neg))
+            elif neg == pos:
+                print("NEUTRAL")
+
+            print ("*************************************")
+            print ("*************************************")
+            count = count + 1
 
             '''
             #write into csv file
