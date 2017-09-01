648f2c8d283c0f5349d7e78eee5c2dae8dda6399
ysalfafara <ysalfafara@gmail.com>
2017-09-01 19:45:30 +0800

    Updated script for TripAdvisor Scraping

--- TripAdvisor/Reviewer.py
+++ TripAdvisor/Reviewer.py
@@ -1,108 +1,148 @@
-# tripadvisor Scrapper - use this one to scrape hotels
-
-# importing libraries
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import pymysql
 from bs4 import BeautifulSoup
 import urllib
 import os
 import re
 from urllib.request import urlopen
 
-# creating CSV file to be used
+#connect to mysql
+db = pymysql.connect("localhost", "root", "", "reviewer")
+cursor = db.cursor()
 
+#open csv file
 file = open(os.path.expanduser(r"~/Desktop/TripAdviser Reviews.csv"), "wb")
 file.write(
-    b"Organization,Address,Reviewer,Review Title,Review,Rating Date,Rating" + b"\n")
+    b"Review,Rating Date,Rating  " + b"\n")
 
-# List the first page of the reviews (ends with "#REVIEWS") - separate the websites with ,
-WebSites = [
-    "https://www.tripadvisor.com.ph/Hotel_Review-g317121-d320846-Reviews-Taal_Vista_Hotel-Tagaytay_Cavite_Province_Calabarzon_Region_Luzon.html#REVIEWS"]
-Checker = "REVIEWS"
-# looping through each site until it hits a break
-num = 0
-for theurl in WebSites:
-    thepage = urlopen(theurl)
-    soup = BeautifulSoup(thepage, "html.parser")
+#extract booking and insert to database
+def tripadvisor():
+    browser = webdriver.Chrome()
+    browser.get('https://www.tripadvisor.com.ph/Hotel_Review-g317121-d320846-Reviews-Taal_Vista_Hotel-Tagaytay_Cavite_Province_Calabarzon_Region_Luzon.html#REVIEWS')
+
+    try:
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="taplc_location_reviews_list_hotels_0"]/div[12]/div')))
+    except TimeoutException:
+        print("Timed out! Waiting for page to load")
+        browser.quit()
+
+
+    moreButton = browser.find_element_by_css_selector("span.taLnk.ulBlueLinks")
+    moreButton.click()
+
+    try:
+        WebDriverWait(browser, 10).until(EC.invisibility_of_element_located((By.XPATH, '//*[@id="review_516865814"]/div/div[2]/div/div[1]/div[3]/div/p/span')))
+    except TimeoutException:
+        print("Timed out! Waiting for more button to load")
+        browser.quit()
+
+    count = 0
+    ta_id = 103
+    html_source = browser.page_source
+    soup = BeautifulSoup(html_source, "html.parser")
     while True:
-        # extract the help count, restaurant review count, attraction review count and hotel review count
-        a = b = 0
-        hotelarray = ""
-        WebSites1 = ""
+        Reviewer_element = browser.find_elements_by_xpath("//*[@class='expand_inline scrname']")
+        Review_title_element = browser.find_elements_by_xpath("//*[@class='noQuotes']")
+        Review_element = browser.find_elements_by_css_selector(".partial_entry:nth-child(1)")
+        Rating_date_element = browser.find_elements_by_xpath("//*[@class='ratingDate relativeDate']")
+        Rating_element = browser.find_elements_by_xpath("//*[@class='rating reviewItemInline']")
 
-        for profile in soup.findAll(attrs={"class": "member_info"}):
-            image = profile.text.replace("\n", "|||||").strip()
-            if image.find("hotel") > 0:
-                counter = image.split("hotel", 1)[0].split("|", 1)[1][-4:].replace("|", "").strip()
-                if len(hotelarray) == 0:
-                    hotelarray = [counter]
-                else:
-                    hotelarray.append(counter)
-            elif image.find("hotel") < 0:
-                if len(hotelarray) == 0:
-                    hotelarray = ["0"]
-                else:
-                    hotelarray.append("0")
+        Reviewer = []
+        Review_title = []
+        Review = []
+        Rating_date = []
+        Rating = []
 
-                # extract the rating count for each user review
+        for x in range(5):
 
-        Organization = soup.find(attrs={"class": "heading_title"}).text.replace('"', ' ').replace('Review of',
-                                                                                                 ' ').strip()
-        Address = soup.findAll(attrs={"class": "locality"})[0].text.replace(',', '').replace('\n', '').strip()
+            Reviewer.append(Reviewer_element[x].text.replace(',', ' ').replace('"', '').replace('"', '').replace('"', '').strip())
+            Review_title.append(Review_title_element[x].text.replace(',', ' ').replace('"', '').replace('"', '').replace('"', '').strip())
+            Review.append(Review_element[x].text.replace(',', ' ').replace('"', '').replace('"', '').replace('"', '').replace('\n', ' ').strip())
+            Rating_date.append(Rating_date_element[x].text.replace('Reviewed', ' ').replace('NEW',' ').replace(',', ' ').strip())
 
-        # Loop through each review on the page
-        for x in range(0, len(hotelarray)):
-            try:
-                Reviewer = soup.findAll(attrs={"class": "expand_inline scrname"})[x].text
-            except:
-                Reviewer = "N/A"
-                continue
+            for i in soup.findAll(attrs={"class": "rating reviewItemInline"}):
 
-            Reviewer = Reviewer.replace(',', ' ').replace('"', '').replace('"', '').replace('"', '').strip()
-            ReviewCount = soup.findAll(attrs={"class": "badgetext"})[x].text.split(' ', 1)[0].strip()
-            ReviewTitle = soup.findAll(attrs={"class": "noQuotes"})[x].text.replace(',', ' ').replace('"', '').replace('"','').replace('"', '').replace('e', 'e').strip()
-            Review = soup.findAll(attrs={"class": "partial_entry"})[x].text.replace(',', ' ').replace('\n', ' ').strip()
-            RatingDate = soup.findAll(attrs={"class": "ratingDate relativeDate"})[x].text.replace('Reviewed', ' ').replace('NEW',' ').replace(',', ' ').strip()
+                if i.find(class_ = "ui_bubble_rating bubble_50"):
+                    rate = str(100)
+                    Rating.append(rate)
+                elif i.find(class_ = "ui_bubble_rating bubble_40"):
+                    rate = str(80)
+                    Rating.append(rate)
+                elif i.find(class_ = "ui_bubble_rating bubble_30"):
+                    rate = str(60)
+                    Rating.append(rate)
+                elif i.find(class_ = "ui_bubble_rating bubble_20"):
+                    rate = str(40)
+                    Rating.append(rate)
+                elif i.find(class_ = "ui_bubble_rating bubble_10"):
+                    rate = str(20)
+                    Rating.append(rate)
 
-            altarray = []
-            for rating in soup.findAll(attrs={"class": "rating reviewItemInline"}):
+            #print at cmd
+            print(Review[count] + Rating[count] + Rating_date[count])
 
-                if rating.find(class_ = "ui_bubble_rating bubble_50"):
-                    Rating = str(50)
-                    altarray.append(Rating)
-                elif rating.find(class_ = "ui_bubble_rating bubble_40"):
-                    Rating = str(40)
-                    altarray.append(Rating)
-                elif rating.find(class_ = "ui_bubble_rating bubble_30"):
-                    Rating = str(30)
-                    altarray.append(Rating)
-                elif rating.find(class_ = "ui_bubble_rating bubble_20"):
-                    Rating = str(20)
-                    altarray.append(Rating)
-                elif rating.find(class_ = "ui_bubble_rating bubble_10"):
-                    Rating = str(10)
-                    altarray.append(Rating)
+            #insert to database (negative reviews)
+            cursor.execute("INSERT INTO CUSTOMER  (REVIEWSITES_ID, CSTMR_REVIEW, CSTMR_RATINGDATE, CSTMR_RATING) values (%s,%s,%s,%s)",
+            (ta_id, str(Review[count]),str(Rating_date[count]),str(Rating[count])))
 
-            Rating = altarray[x]
+            db.commit()
+            count = count + 1
 
-            Record = Organization + "," + Address + "," + Reviewer + "," + ReviewTitle + "," + Review + "," + RatingDate + "," + Rating
-            if Checker == "REVIEWS":
-                file.write(bytes(Record, encoding="ascii", errors='ignore')  + b"\n")
+            if count == 5:
+                break
 
+            '''
+            #write into csv file
+            Record = Review[count] + "," + Rating_date[count] + "," + Rating[count]
+
+            file.write(bytes(Record, encoding="ascii", errors='ignore')  + b"\n")
+            '''
+'''
+        count = 0
         link = soup.find_all(attrs={"class": "nav next taLnk "})
-        print(Organization)
-        print(link)
-        if num == 15:
+        if link == False:
             break
-        else:
-            num = num + 5
-            WebSites1 = "http://www.tripadvisor.com.ph" + "/Hotel_Review-g317121-d320846-Reviews-or" + str(num) + "-Taal_Vista_Hotel-Tagaytay_Cavite_Province_Calabarzon_Region_Luzon.html#REVIEWS"
-            page = urlopen(WebSites1)
-            soup = BeautifulSoup(page, "html.parser")
-            print(WebSites1)
-            Checker = WebSites1[-7:]
 
-file.close()
+        else:
+            try:
+                WebDriverWait(browser, 200).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="taplc_location_reviews_list_hotels_0"]/div[12]/div')))
+                NextButton = browser.find_element_by_css_selector("span.nav.next.taLnk ")
+                NextButton.click()
+                html_source = browser.page_source
+                soup = BeautifulSoup(html_source, "html.parser")
+
+                try:
+                    WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "span.taLnk.ulBlueLinks")))
+                except Exception:
+                    print("Timed out! Waiting for page to load")
+                    browser.quit()
+
+
+                if WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#taplc_location_reviews_list_hotels_0"))):
+                    WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "span.taLnk.ulBlueLinks")))
+                    morebutton = browser.find_element_by_css_selector("span.taLnk.ulBlueLinks")
+                    morebutton.click()
+
+                    try:
+                        WebDriverWait(browser, 200).until(EC.invisibility_of_element_located((By.XPATH, '//*[@id="review_516865814"]/div/div[2]/div/div[1]/div[3]/div/p/span')))
+                        WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".expand_inline.scrname")))
+                        WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".noQuotes")))
+                        WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".partial_entry")))
+                        WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".ratingDate.relativeDate")))
+                        WebDriverWait(browser, 200).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".rating.reviewItemInline")))
+                    except Exception:
+                        print("Timed out! Waiting for more button to load")
+                        browser.quit()
+
+            except Exception:
+                print("Timed out! Waiting for next button to load")
+                browser.quit()
+'''
+
+if __name__ == "__main__":
+  tripadvisor()
